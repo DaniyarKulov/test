067471bfc738801c1019643d7107c76fc9fb926f
Daniyar Kulov <kulovdaniyar77@gmail.com>
2023-07-06 01:58:57 +0600

    feat: add delete columns

--- src/app/users/components/table/table.component.ts
+++ src/app/users/components/table/table.component.ts
@@ -5,11 +5,10 @@ import { Subscription, tap } from 'rxjs';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort, Sort } from '@angular/material/sort';
-import { MatCheckboxChange } from '@angular/material/checkbox';
 import { SortUsersService } from '../../services/sort-users.service';
 import { UsersFacadeService } from '../../services/users-facade.service';
 import { UserData } from '../../models/user-data.model';
-import { localeStringComporator } from '../../utils/locale-string-comporator.const';
+import { CollumnsName } from '../../models/collumnsName.type';
 
 @Component({
   selector: 'agn-table',
@@ -20,9 +19,7 @@ import { localeStringComporator } from '../../utils/locale-string-comporator.con
 export class TableComponent implements OnInit, OnDestroy {
   public users!: MatTableDataSource<UserData>;
   public isVision = false;
-  public currentIndex: number | null = null;
-  public indexes:number[] = [];
-  public displayedColumns = [
+  public columnsName: CollumnsName[] = [
     'name',
     'age',
     'picture',
@@ -34,7 +31,7 @@ export class TableComponent implements OnInit, OnDestroy {
     'favorite_fruit',
     'isActive',
   ];
-  public displayedCheckbox = [
+  public checkboxName: CollumnsName[] = [
     'name',
     'age',
     'picture',
@@ -44,8 +41,20 @@ export class TableComponent implements OnInit, OnDestroy {
     'address',
     'tags',
     'favorite_fruit',
-    'isActive',
   ];
+
+  public isCheckedColumnsName = {
+    name: false,
+    age: false,
+    picture: false,
+    company: false,
+    balance: false,
+    email: false,
+    address: false,
+    tags: false,
+    favorite_fruit: false,
+    isActive: false,
+  };
   public collumName = '';
   @ViewChild(MatPaginator) public paginator!: MatPaginator;
   @ViewChild(MatSort) public sort!: MatSort;
@@ -53,7 +62,7 @@ export class TableComponent implements OnInit, OnDestroy {
 
   constructor(
     private usersFacadeService: UsersFacadeService,
-    private usersSortService:SortUsersService,
+    private usersSortService: SortUsersService,
   ) {
   }
 
@@ -66,7 +75,6 @@ export class TableComponent implements OnInit, OnDestroy {
       this.users = new MatTableDataSource(data);
       this.users.paginator = this.paginator;
       this.users.sort = this.sort;
-      this.indexes = Array.from({ length: data.length }, (_, index) => index);
     }));
   }
 
@@ -83,31 +91,17 @@ export class TableComponent implements OnInit, OnDestroy {
     this.subs.unsubscribe();
   }
 
-  public sortUsers(sortCriteria: Sort):void {
+  public sortUsers(sortCriteria: Sort): void {
     this.usersSortService.sortUsers(sortCriteria);
   }
 
-  // public onVision(index:number):void {
-  //   this.currentIndex = index;
-  //   const currentIndex = this.indexes.indexOf(index);
-  //   if (currentIndex > -1) {
-  //     this.indexes.splice(currentIndex, 1);
-  //   } else {
-  //     this.indexes.push(index);
-  //   }
-  //   this.isVision = !this.isVision;
-  // }
-
-  public onVision():void {
+  public onVision(): void {
     this.isVision = !this.isVision;
   }
 
-  public checked(columnName: string): void {
+  public checked(columnName: CollumnsName): void {
     this.collumName = columnName;
-  }
-
-  public localComparator(string:string):boolean {
-    return !!localeStringComporator(this.collumName, string);
+    this.isCheckedColumnsName[columnName] = !this.isCheckedColumnsName[columnName];
   }
 }
 
---
+++ src/app/users/models/collumnsName.type.ts
@@ -0,0 +1,11 @@
+export type CollumnsName =
+  'name'
+  | 'picture'
+  | 'age'
+  | 'company'
+  | 'balance'
+  | 'email'
+  | 'address'
+  | 'tags'
+  | 'favorite_fruit'
+  | 'isActive';
